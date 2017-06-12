6904b39a9d98afe2c58ecccda4244b587ea08114
JonathanCowling <09cowlingj@gmail.com>
2017-06-12 15:16:40 +0100

    the first exercise - hello world is no longer an example

--- examples/hello_world.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-################################################################################
-# hello_world.py
-
-# use the print function to say "hello world"
-print("hello World!")
---
+++ exercises/hello_world.py
@@ -0,0 +1,4 @@
+# lines starting with a "#" symbol are comments that wont be read by the interpreter
+
+# use the print function to say "hello world"
+# n.b make sure that you are using the python 3 interpreter

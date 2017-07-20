9c47f967d2891b9653e02f74bb3fc762f1e8cfe1
jonathan-cowling <cowlingj@users.noreply.github.com>
2017-07-20 20:42:48 +0100

    Create repeat_me.py

---
+++ solutions/repeat_me.py
@@ -0,0 +1,17 @@
+# repeat_me.py
+
+"""
+Repeat what the user inputs
+"""
+
+# get the user input
+var user_input = input("Say something > ")
+
+# let the user know we are going to repeat there input
+print("You said:")
+
+# print what the user said
+print(user_input)
+
+# print a goodbye message
+print("goodbye")

02c9cdcc0d7015e739f12f27c1d87efe8219ceca
jonathan-cowling <cowlingj@users.noreply.github.com>
2017-07-20 20:41:24 +0100

    Create repeat_me.py

---
+++ exercises/repeat_me.py
@@ -0,0 +1,13 @@
+# repeat_me.py
+
+"""
+Repeat what the user inputs
+"""
+
+# get the user input
+
+# let the user know we are going to repeat there input
+
+# print what the user said
+
+# print a goodbye message
